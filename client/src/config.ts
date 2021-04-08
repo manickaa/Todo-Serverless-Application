e16545111f5c0f297f13758b5a9fa9159374d280
Aishwarya Manicka <manickaa@oregonstate.edu>
2021-04-07 19:32:16 -0700

    Added Auth0 configuration

--- client/src/config.ts
+++ client/src/config.ts
@@ -4,7 +4,7 @@ export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-test-endpoint.us.auth0.com',            // Auth0 domain
+  clientId: 'UPFzmd23O8O0fjqs0lArqnHmwnVcm40x',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
