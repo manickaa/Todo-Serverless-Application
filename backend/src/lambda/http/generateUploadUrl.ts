56b1079f7b29ba372961596614cd1df5c0d1be6c
Aishwarya Manicka <manickaa@oregonstate.edu>
2021-04-09 16:32:58 -0700

    generate signed url handler

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -1,10 +1,23 @@
 import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import {generateUploadUrl} from '../../businessLogic/todoLogic'
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const todoId = event.pathParameters.todoId
+  //const todoId = event.pathParameters.todoId
 
   // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  return undefined
+  const signedURL = await generateUploadUrl(event)
+  console.log('Signed url', signedURL)
+
+  return {
+    statusCode: 202,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({
+      uploadUrl: signedURL
+    })
+  }
 }
