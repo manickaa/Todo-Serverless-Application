70d25bbe73cdf34ebc915cd1b6afa84910c85881
Aishwarya Manicka <manickaa@oregonstate.edu>
2021-04-09 11:57:16 -0700

    Updated updateTodo handler

--- backend/src/lambda/http/updateTodo.ts
+++ backend/src/lambda/http/updateTodo.ts
@@ -3,11 +3,30 @@ import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
 
 import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+import { updateTodo } from '../../businessLogic/todoLogic'
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
   const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
-
+  const updatedItem = await updateTodo(event, updatedTodo)
   // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-  return undefined
+  
+  if(!updatedItem) {
+    return {
+      statusCode: 404,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: `Cannot find todoItem with id: ${todoId}`
+    }
+  }
+  
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: `Updated item with id : ${todoId}`
+  }
 }
