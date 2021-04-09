5d0275460dae6962ff13a1ab9487107b8b61c599
Aishwarya Manicka <manickaa@oregonstate.edu>
2021-04-09 12:02:15 -0700

    Updated deleteTodo handler

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -1,10 +1,28 @@
 import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
-
+import {deleteTodo} from '../../businessLogic/todoLogic'
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
 
   // TODO: Remove a TODO item by id
-  return undefined
+  const deleted = await deleteTodo(event)
+
+  if(!deleted) {
+    return {
+      statusCode: 404,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: `Cannot delete todoItem with id: ${todoId}. Id not found`
+    }
+  }
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: `Deleted item with id : ${todoId}`
+  }
 }
